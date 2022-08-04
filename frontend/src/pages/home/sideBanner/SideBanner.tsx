12d5ba4da89855e8997b707e38aacfbcf69a6903
JoshuajRodrigues <rodrigues.joshr@gmail.com>
2022-08-05 00:22:50 +0530

    loitte removed

--- frontend/src/pages/home/sideBanner/SideBanner.tsx
+++ frontend/src/pages/home/sideBanner/SideBanner.tsx
@@ -1,23 +1,11 @@
 import { FunctionComponent } from "react";
 import './SideBanner.scss'
-import Lottie from 'react-lottie'
-import animationData from '../../../lotties/hero.json'
+
 const SideBanner: FunctionComponent<{}> = () => {
-  const defaultOptions = {
-    loop: false,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice"
-    }
-  };
+
   return (
     <>
-      <Lottie
-       options={defaultOptions}
-       height={410}
-       width={410}
-      />
+    
     </>
   );
 };
