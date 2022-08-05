30db7a79969ee6313c5a49e0f3e576a27093b4b0
Joshua <jrodrigues@teknorix.com>
2022-08-05 16:24:56 +0530

    lotties integrated

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 import { appRoutes } from "../../AppConstants";
 import Login from "../login/Login";
 
-
+import lottieSrc from '../../lotties/hero-signin.json'
 import SideBanner from "./sideBanner/SideBanner";
 const Home: FunctionComponent<{}> = () => {
   return (
     <div className="grid-container">
       <div className="item-1">
-        <SideBanner />
+        <SideBanner loop={true} src={lottieSrc} />
       </div>
       <div className="item-2">
         <h2>Welcome to <span className="app-name">Kognitive</span> , <br /> your cbt journal.🤍 </h2>
--- frontend/src/pages/home/sideBanner/SideBanner.tsx
+++ frontend/src/pages/home/sideBanner/SideBanner.tsx
@@ -1,11 +1,18 @@
+import { Player } from "@lottiefiles/react-lottie-player";
 import { FunctionComponent } from "react";
 
 
-const SideBanner: FunctionComponent<{}> = () => {
+const SideBanner: FunctionComponent<{ src: string | object, loop: boolean }> = ({ src, loop }) => {
 
   return (
     <>
-    
+      <Player
+        keepLastFrame
+        autoplay
+        loop={loop}
+        src={src}
+      />
+
     </>
   );
 };
