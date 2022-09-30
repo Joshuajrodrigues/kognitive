e7a9881dec6c19b98c8458b75bd26b7f38e9b4f3
Joshua <jrodrigues@teknorix.com>
2022-09-30 17:04:20 +0530

    css fixes on welcome

--- frontend/src/pages/Welcome.tsx
+++ frontend/src/pages/Welcome.tsx
@@ -1,27 +1,8 @@
-import {
-  ArrowLeftIcon,
-  RepeatClockIcon,
-  CalendarIcon,
-  AddIcon,
-  TimeIcon,
-} from "@chakra-ui/icons";
-import {
-  Box,
-  Button,
-  Center,
-  Divider,
-  Flex,
-  Icon,
-  IconButton,
-  SimpleGrid,
-  Text,
-} from "@chakra-ui/react";
+import { CalendarBlank, Clock, ClockCounterClockwise, NotePencil } from "phosphor-react";
 import { FunctionComponent, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { appRoutes } from "../AppConstants";
-import {CalendarBlank,Clock } from "phosphor-react";
 import useUser from "../hooks/useUser";
-import hellocat from "../lotties/hellocat.json";
 const Welcome: FunctionComponent<{}> = () => {
   const user = useUser((state) => state.user);
   const [dateTime, setDateTime] = useState(new Date());
@@ -36,69 +17,55 @@ const Welcome: FunctionComponent<{}> = () => {
       <div className="welcome-grid-container">
         <div className="welcome-main-box"
         >
-          <Flex alignItems={"center"} pl={"4"} pr={4} pt={4}>
-          <CalendarBlank alt="Calender icon"  color={"#805ad5"} size={32} />
-            <Text color={"purple.500"} mr={2}>
+          <div className="flex-date-time">
+            <CalendarBlank alt="Calender icon" color={"#805ad5"} size={24} />{"  "}
+            <div className="intro-text-light">
               {dateTime.toLocaleDateString("en-IN", {
                 day: "numeric",
                 month: "short",
                 year: "numeric",
               })}
-            </Text>
+            </div>{"  "}
 
-            <Clock alt="clock icon" color={"#805ad5"}size={32} />
+            <Clock alt="clock icon" color={"#805ad5"} size={24} />{"  "}
             
-            <Text color={"purple.500"}>
+            <div className="intro-text-light">
               {dateTime.toLocaleTimeString("en-IN", {
                 hour: "numeric",
                 minute: "numeric",
                 hour12: true,
               })}
-            </Text>
-          </Flex>
+            </div>
+          </div>
 
-          <Text
-            fontWeight={"semibold"}
-            color={"purple.500"}
-            fontSize={"lg"}
-            m={4}
-          >
+          <div className="intro-text">
             Welcome {user.user_metadata?.name}, how are you feeling ?
-          </Text>
-          <SimpleGrid columns={1}>
+          </div>
+          <div>
             <div className="lottie-container">
               <img src="./checkin.svg" alt="" className="hero-image" />
             </div>
-            <Flex m={2} justifyContent={"center"} direction={{"base":"column","sm":"column",md:"row",lg:"row"}}>
-              <Button
-              m={2}
-                alignItems={"center"}
-                alignSelf={"center"}
-                w={"120px"}
-                colorScheme="purple"
+            <div className="welcome-button-container">
+              <button
+                className="normal-button"
                 aria-label="Make a cbt entry"
                 onClick={() => navigate(appRoutes.cbtForm)}
               >
-                <AddIcon w={3} mr={1} />
+                <NotePencil size={16} />
                New entry
-              </Button>
-              <Button
-                m={2}
-                alignItems={"center"}
-                alignSelf={"center"}
-                w={"120px"}
-                colorScheme="purple"
+              </button>
+              <button
+                className="normal-button"
                 aria-label="Make a cbt entry"
                 onClick={() => navigate(appRoutes.historicalSubmites)}
               >
-                <RepeatClockIcon w={3} mr={1} />
+                <ClockCounterClockwise size={16} />
                 View histroy
-              </Button>
-            </Flex>
-          </SimpleGrid>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-      <Divider />
     </>
   );
 };
